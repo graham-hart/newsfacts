187845fd6db3dd69f74547e48bfc1093e9578c9a
Graham Hart <graham@harts.net>
2021-02-15 16:36:14 -0800

    Routing
    KNOWN BUGS: HTML does not change, only route

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {},
+};
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/Search.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Search</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  components: {},
+  data() {},
+};
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
