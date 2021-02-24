cb3deeb7d0ab8b8752c0a80c0796a7a6281e3639
Graham Hart <graham@harts.net>
2021-02-24 13:39:14 -0800

    Simple Email Login FIX LATER

---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <span id="login" :v-if="getEmail() == null">
+      <input name="email" type="text" placeholder="Email..." v-model="email" />
+      <button @click="setEmail()">Submit</button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: null,
+    };
+  },
+  methods: {
+    getEmail() {
+      return localStorage.getItem("email");
+    },
+    setEmail() {
+      localStorage.setItem("email", this.email);
+      this.$store.commit("createUser", this.email);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
