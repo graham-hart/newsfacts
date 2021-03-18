40f1cf728e890f2cca3a259de59542735d9ecedc
Graham Hart <graham@harts.net>
2021-03-18 00:46:28 -0700

    Basic Footer Content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,46 @@
 <template>
   <div id="footer">
-    Nisi non laboris reprehenderit culpa eiusmod ea veniam nostrud consequat
-    cupidatat tempor. Officia esse irure et consectetur est consequat. Laborum
-    duis magna veniam deserunt labore non ullamco qui laboris sunt pariatur. Ad
-    tempor veniam aliquip sint dolore sit duis amet incididunt ullamco do ut.
-    Consequat nisi dolor elit adipisicing fugiat ad excepteur ipsum irure.
-    Nostrud aute commodo amet consectetur duis elit ex adipisicing
-    reprehenderit. Proident aute magna excepteur deserunt. Ipsum fugiat ex id
-    aute. Minim proident ipsum aute non elit dolore veniam quis consectetur ad
-    deserunt enim sunt incididunt.
+    <router-link
+      v-for="link in links"
+      :key="link.name"
+      :to="link.path"
+      tag="span"
+      class="footer-link"
+      >{{ link.name }}<br
+    /></router-link>
   </div>
 </template>
 <script>
 export default {
   name: "Footer",
+  data() {
+    return {
+      links: [
+        { name: "Home", path: "/" },
+        { name: "About", path: "/about" },
+        { name: "Contact", path: "/contact" },
+        { name: "Sites", path: "/sites" },
+      ],
+    };
+  },
 };
 </script>
 <style scoped>
 #footer {
   align-self: flex-end;
   margin-bottom: auto;
-  padding: 20px;
-  line-height: 40px;
+  padding: 20px 40px 20px 40px;
   background-color: rgb(99, 32, 185);
   column-count: 3;
   column-gap: 40px;
-  color: rgb(175, 183, 189);
-  font-weight: bold;
+  line-height: 1.2rem;
+}
+.footer-link {
+  color: rgb(255, 255, 255);
+  font-weight: 300;
+  cursor: pointer;
+}
+.footer-link:hover {
+  text-decoration: underline;
 }
 </style>
