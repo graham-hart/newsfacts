cd8e623dc8a3d9b39f3ce88cd580beeb321e4ae6
Graham Hart <graham@harts.net>
2021-04-09 17:44:35 -0700

    Voting Criteria

--- src/router/index.js
+++ src/router/index.js
@@ -5,56 +5,54 @@ import SitePage from '@/views/SitePage.vue';
 import About from '@/views/About.vue';
 import Contact from '@/views/Contact.vue';
 import PageNotFound from '@/views/PageNotFound.vue';
-// import VotingCriteria from '@/views/VotingCriteria.vue';
+import VotingCriteria from '@/views/VotingCriteria.vue';
 import LoginCallback from '@/views/LoginCallback.vue';
 
 Vue.use(Router);
 
 export default new Router({
-	mode: "history",
-	scrollBehavior(to, from, savedPosition) {
-		if (savedPosition) {
-			return savedPosition;
-		} else {
-			return { x: 0, y: 0, };
-		}
-	},
-	routes: [
-		{
-			path: "/",
-			name: "Home",
-			component: Home
-		},
-		{
-			path: "/sites/:site",
-			name: "Site",
-			component: SitePage
-		},
-		{
-			path: "/about",
-			name: "About",
-			component: About
-		},
-		{
-			path: "/contact",
-			name: "Contact",
-			component: Contact
-		},
-		// {
-		// 	path: "/criteria",
-		// 	name: "VotingCriteria",
-		// 	component: VotingCriteria,
-		// },
-		{
-			path: "/login-callback",
-			name: "LoginCallback",
-			component: LoginCallback,
-		},
-		{
-			path: "/*",
-			name: "404",
-			component: PageNotFound,
-		}
-	]
+    mode: "history",
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { x: 0, y: 0, };
+        }
+    },
+    routes: [{
+            path: "/",
+            name: "Home",
+            component: Home
+        },
+        {
+            path: "/sites/:site",
+            name: "Site",
+            component: SitePage
+        },
+        {
+            path: "/about",
+            name: "About",
+            component: About
+        },
+        {
+            path: "/contact",
+            name: "Contact",
+            component: Contact
+        },
+        {
+            path: "/criteria",
+            name: "VotingCriteria",
+            component: VotingCriteria,
+        },
+        {
+            path: "/login-callback",
+            name: "LoginCallback",
+            component: LoginCallback,
+        },
+        {
+            path: "/*",
+            name: "404",
+            component: PageNotFound,
+        }
+    ]
 });
-
--- src/views/VotingCriteria.vue
+++ src/views/VotingCriteria.vue
@@ -1,13 +1,34 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div id="body">
+      <h1>Bias</h1>
+      <hr />
+      <p>
+        Bias is ranked on a scale of -5 - 5, where 0 is completely unbiased and
+        5 is incredibly biased. Negative numbers means Progressive bias, and
+        Positive means Conservative bias.
+      </p>
+      <h1>Credibility</h1>
+      <hr />
+      <p>
+        Credibility is ranked 0 - 5, where 0 is completely untrustworthy and 5
+        is incredibly untrustworthy.
+      </p>
+    </div>
+  </div>
 </template>
-<script>
-export default {
-  name: "",
-  data() {
-    return { tmp: 1 };
-  },
-};
-</script>
 <style scoped>
+#body {
+  width: 90%;
+  margin: 50px auto;
+}
+hr {
+  margin: 20px auto;
+  border: 2px solid #ddd;
+}
+p {
+  font-size: 1.2rem;
+  line-height: 5rem;
+  font-family: "Roboto", "Times";
+}
 </style>
